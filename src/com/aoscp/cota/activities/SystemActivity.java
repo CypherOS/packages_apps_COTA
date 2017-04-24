9ba2601c69d67e9f217d1b3a4b8ac08c04b8cf4d
Chris <chris@cypheros.co>
2017-04-24 19:53:11 -0400

    Cota: Start install notification when download completes
    Change-Id: Ief31ced95b789af39c92954b1857b5b5c8f54ba7

--- src/com/aoscp/cota/activities/SystemActivity.java
+++ src/com/aoscp/cota/activities/SystemActivity.java
@@ -304,7 +304,6 @@ public class SystemActivity extends AppCompatActivity implements UpdaterListener
             mState = STATE_INSTALL;
             updateMessages((PackageInfo) null);
             addFile(uri, md5);
-			mNotifUtils.onCompleted(getContext());
         } else {
             mState = STATE_CHECK;
             mRomUpdater.check(true);
