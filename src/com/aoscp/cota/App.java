8e91a610ec3e8bd368af25ea6c0f74a124cbeacb
Chris <chris@cypheros.co>
2017-04-24 21:33:41 -0400

    Cota: New notification logic
    Change-Id: Ief31ced95b789af39c92954b1857b5b5c8f54ba7

--- src/com/aoscp/cota/App.java
+++ src/com/aoscp/cota/App.java
@@ -1,13 +1,19 @@
 package com.aoscp.cota;
 
+import android.app.Activity;
 import android.app.Application;
+import android.os.Bundle;
 
 import org.piwik.sdk.DownloadTracker;
 import org.piwik.sdk.PiwikApplication;
 import org.piwik.sdk.TrackHelper;
 
-public class App extends PiwikApplication {
-    private static App sApplication;
+import com.aoscp.cota.activities.SystemActivity;
+
+public class App extends Application implements PiwikApplication,
+        Application.ActivityLifecycleCallbacks {
+			
+	private boolean mSystemUpdateInView;
 
     @Override
     public String getTrackerUrl() {
@@ -21,12 +27,45 @@ public class App extends PiwikApplication {
 
     @Override
     public void onCreate() {
-        super.onCreate();
-
-        sApplication = this;
+		mSystemUpdateInView = false;
+        registerActivityLifecycleCallbacks(this);
     }
 
-    public static App getApplication() {
-        return sApplication;
+    @Override
+    public void onActivityCreated (Activity activity, Bundle savedInstanceState) {
+    }
+
+    @Override
+    public void onActivityDestroyed (Activity activity) {
+    }
+
+    @Override
+    public void onActivityPaused (Activity activity) {
+    }
+
+    @Override
+    public void onActivityResumed (Activity activity) {
+    }
+
+    @Override
+    public void onActivitySaveInstanceState (Activity activity, Bundle outState) {
+    }
+
+    @Override
+    public void onActivityStarted (Activity activity) {
+        if (activity instanceof SystemActivity) {
+            mSystemUpdateInView = true;
+        }
+    }
+
+    @Override
+    public void onActivityStopped (Activity activity) {
+        if (activity instanceof SystemActivity) {
+            mSystemUpdateInView = false;
+        }
+    }
+
+    public boolean isSystemUpdateInView() {
+        return mSystemUpdateInView;
     }
 }
