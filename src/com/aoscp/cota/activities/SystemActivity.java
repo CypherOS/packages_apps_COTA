e069fb2b9ddd1fa117c049353ce71484c1f62d5c
Chris <chris@cypheros.co>
2017-04-26 21:14:26 -0400

    Cota: Inherit scrollview for update description
    Instead of including our dynamic update messages with
    static text, let's branch this out into it's own
    scroll based textview. This way we can create a markdown
    of changes and/or push more information to the user without
    crowding the main layout.
    
    Change-Id: I38a27d28cff687394d59e54c2ef1a84729a5c028

--- src/com/aoscp/cota/activities/SystemActivity.java
+++ src/com/aoscp/cota/activities/SystemActivity.java
@@ -15,6 +15,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.format.Formatter;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -72,6 +73,7 @@ public class SystemActivity extends AppCompatActivity implements UpdaterListener
 
     private CoordinatorLayout mCoordinatorLayout;
     private TextView mMessage;
+	private TextView mHighlights;
 	private TextView mSize;
     private Button mButton;
     private TextView mHeader;
@@ -87,6 +89,8 @@ public class SystemActivity extends AppCompatActivity implements UpdaterListener
         mMessage = (TextView) findViewById(R.id.message);
 		mSize = (TextView) findViewById(R.id.size);
         mButton = (Button) findViewById(R.id.action);
+		mHighlights = (TextView) findViewById(R.id.highlights);
+        mHighlights.setMovementMethod(new ScrollingMovementMethod());
 		
 		bar = (ProgressBar) findViewById(R.id.progress_bar);
 
@@ -188,6 +192,7 @@ public class SystemActivity extends AppCompatActivity implements UpdaterListener
                             mDeviceUtils.getVersionDisplay()));
                     mButton.setText(R.string.no_updates_check);
 					bar.setVisibility(View.INVISIBLE);
+					mHighlights.setVisibility(View.INVISIBLE);
                     Log.v(TAG, "updateMessages:STATE_CHECK = mUpdatePackage != null");
                 }
                 Log.v(TAG, "updateMessages:STATE_CHECK = mUpdatePackage == null");
@@ -198,8 +203,10 @@ public class SystemActivity extends AppCompatActivity implements UpdaterListener
                     mMessage.setText(String.format(
                             getResources().getString(R.string.update_found_text),
 							mUpdatePackage.getVersion(),
-							mDeviceUtils.getModel(),
-                            mUpdatePackage.getText()));
+							mDeviceUtils.getModel()));
+					mHighlights.setText(String.format(
+                            getResources().getString(R.string.update_found_highlights),
+							mUpdatePackage.getText()));
 					mSize.setText(String.format(
                             getResources().getString(R.string.update_found_size),
                             Formatter.formatShortFileSize(this, Long.decode(mUpdatePackage.getSize()))));
@@ -214,6 +221,7 @@ public class SystemActivity extends AppCompatActivity implements UpdaterListener
                 mMessage.setText(R.string.downloading_text);
                 mButton.setText(R.string.downloading_cancel);
 				bar.setVisibility(View.VISIBLE);
+				mHighlights.setVisibility(View.INVISIBLE);
                 Log.v(TAG, "updateMessages:STATE_DOWNLOADING = " + (R.string.downloading_text));
                 break;
             case STATE_ERROR:
@@ -221,6 +229,7 @@ public class SystemActivity extends AppCompatActivity implements UpdaterListener
                 mMessage.setText(R.string.download_failed_text);
                 mButton.setText(R.string.no_updates_check);
 				bar.setVisibility(View.INVISIBLE);
+				mHighlights.setVisibility(View.INVISIBLE);
                 Log.v(TAG, "updateMessages:STATE_ERROR");
                 break;
             case STATE_INSTALL:
@@ -228,6 +237,7 @@ public class SystemActivity extends AppCompatActivity implements UpdaterListener
                 mMessage.setText(R.string.install_text);
                 mButton.setText(R.string.install_action);
 				bar.setVisibility(View.INVISIBLE);
+				mHighlights.setVisibility(View.VISIBLE);
                 Log.v(TAG, "updateMessages:STATE_INSTALL");
                 break;
         }
