0d6f85ada9c31330980a119196aebf06aa282fe9
Chris <chris@cypheros.co>
2017-07-13 04:54:57 -0400

    Cota: add system interface
    - Ported over the layouts from our old base
    - Removed redundant code and infrastructure
    - Cleaned up some variables
    - Ported Button OnClickListener for state changes
    - Adapted ported code to OpenDeltas infrastructure
    
    PS2: Cleanup onClickListeners
    Update json fields
    Rework layout changes
    Port lastChecked method
    PS3: Readapt properties to fit our OS
    Match zips with new prefix
    Remove android version property(Our zips don't contain these)
    Fixed server file path for script and downloads
    PS4: Remove config to detect official version. It's causing issues and
    we
    can set this on build if neccessary
    
    Change-Id: Ic507a7069d72c1d5af3ebd13297049b888a9edbf

--- Android.mk
+++ Android.mk
@@ -7,12 +7,15 @@ LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
+    frameworks/support/v7/appcompat/res \
     frameworks/support/v7/cardview/res
 
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-appcompat
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-cardview
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-v4
 
 LOCAL_AAPT_FLAGS := --auto-add-overlay \
+    --extra-packages android.support.v7.appcompat \
     --extra-packages android.support.v7.cardview
 
 LOCAL_JNI_SHARED_LIBRARIES := libopendelta
