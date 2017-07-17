3fc9de97ba7af7f80881acf0faabc08d10bd0ba9
Mati Halperin <matias.halperin@gmail.com>
2017-07-17 19:13:51 -0300

    Cota: Cleanup after rebase and improvements
    Change-Id: I30d9ddeb8ee337ffde27e010cca09fc242b48a7e

--- Android.mk
+++ Android.mk
@@ -1,26 +1,21 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := COTA
 LOCAL_MODULE_TAGS := optional
 LOCAL_PRIVILEGED_MODULE := true
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
-    frameworks/support/v7/appcompat/res \
-    frameworks/support/v7/cardview/res
+LOCAL_PROGUARD_FLAG_FILES := proguard-project.txt
 
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-appcompat
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-cardview
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-v4
 
-LOCAL_AAPT_FLAGS := --auto-add-overlay \
-    --extra-packages android.support.v7.appcompat \
-    --extra-packages android.support.v7.cardview
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_AAPT_FLAGS := --auto-add-overlay
 
 LOCAL_JNI_SHARED_LIBRARIES := libopendelta
 LOCAL_REQUIRED_MODULES := libopendelta
-LOCAL_PROGUARD_FLAG_FILES := proguard-project.txt
 
 include $(BUILD_PACKAGE)
 
