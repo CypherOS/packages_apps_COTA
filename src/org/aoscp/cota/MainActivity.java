9899d6caa1eadf205bb37f4362a1ba167a7c488d
Mati Halperin <matias.halperin@gmail.com>
2017-08-08 18:17:24 -0300

    Cota: Cleanup after rebase and improvements
    Change-Id: Idf34013ed38cfe1d36952cb7501f77f1b3cb4f84

--- src/org/aoscp/cota/MainActivity.java
+++ src/org/aoscp/cota/MainActivity.java
@@ -327,7 +327,7 @@ public class MainActivity extends Activity {
     protected void onStart() {
         super.onStart();
         registerReceiver(updateReceiver, updateFilter);
-        overridePendingTransition(R.anim.slide_next_in, R.anim.slide_next_out);
+        overridePendingTransition(R.anim.suw_slide_next_in, R.anim.suw_slide_next_out);
     }
 
     @Override
@@ -339,7 +339,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
-        overridePendingTransition(R.anim.slide_next_in, R.anim.slide_next_out);
+        overridePendingTransition(R.anim.suw_slide_next_in, R.anim.suw_slide_next_out);
         UpdateService.startUpdate(this);
     }
 
