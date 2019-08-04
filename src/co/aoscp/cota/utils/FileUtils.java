093a2e984680d454b15b1f28350da99bc7ba193c
Chris Crump <chris@cypheros.co>
2019-08-04 18:11:47 -0400

    Cota: Initial support for A/B Seamless updates
    Change-Id: I3388b7538e28f01f1244966efd4977cbe4bbd327

--- src/co/aoscp/cota/utils/FileUtils.java
+++ src/co/aoscp/cota/utils/FileUtils.java
@@ -22,6 +22,7 @@ package co.aoscp.cota.utils;
 import android.content.Context;
 import android.os.Environment;
 import android.os.StatFs;
+import android.util.Log;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -29,9 +30,14 @@ import java.io.InputStream;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.Scanner;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 public class FileUtils {
+
+    private static final String TAG = "FileUtils";
 
     public static final String DOWNLOAD_PATH = new File(Environment
             .getExternalStorageDirectory(), "Updates/").getAbsolutePath();
@@ -320,5 +326,35 @@ public class FileUtils {
     private static boolean folderExists(String path) {
         File f = new File(path);
         return f.exists() && f.isDirectory();
+    }
+
+    /**
+     * Get the offset to the compressed data of a file inside the given zip
+     *
+     * @param zipFile input zip file
+     * @param entryPath full path of the entry
+     * @return the offset of the compressed, or -1 if not found
+     * @throws IllegalArgumentException if the given entry is not found
+     */
+    public static long getZipEntryOffset(ZipFile zipFile, String entryPath) {
+        // Each entry has an header of (30 + n + m) bytes
+        // 'n' is the length of the file name
+        // 'm' is the length of the extra field
+        final int FIXED_HEADER_SIZE = 30;
+        Enumeration<? extends ZipEntry> entries = zipFile.entries();
+        long offset = 0;
+        while (entries.hasMoreElements()) {
+            ZipEntry entry = entries.nextElement();
+            int n = entry.getName().length();
+            int m = entry.getExtra() == null ? 0 : entry.getExtra().length;
+            int headerSize = FIXED_HEADER_SIZE + n + m;
+            offset += headerSize;
+            if (entry.getName().equals(entryPath)) {
+                return offset;
+            }
+            offset += entry.getCompressedSize();
+        }
+        Log.e(TAG, "Entry " + entryPath + " not found");
+        throw new IllegalArgumentException("The given entry was not found");
     }
 }
